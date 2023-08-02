e417285685e9bfa9725c89536ba1b6baea576817
John Whitton <john@johnwhitton.com>
2023-08-01 17:23:32 -0700

    Step-2 name package

--- factors/host/src/main.rs
+++ factors/host/src/main.rs
@@ -1,7 +1,7 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method
 // is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
-use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
+use factors_methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
 use risc0_zkvm::{
     default_executor_from_elf,
     serde::{from_slice, to_vec},
