1e9a1cd785276711eb8b4d5a57bcc02aff369471
John Whitton <john@johnwhitton.com>
2023-08-01 17:10:05 -0700

    Save working version before new build

--- factors/src/main.rs
+++ factors/src/main.rs
@@ -1,45 +1,28 @@
-use factors_methods::{MULTIPLY_ELF, MULTIPLY_ID};
-use risc0_zkvm::{
-    default_executor_from_elf,
-    serde::{from_slice, to_vec},
-    ExecutorEnv,
-};
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use factors::multiply_factors;
+use factors_methods::MULTIPLY_ID;
 
 fn main() {
-    let a: u64 = 17;
-    let b: u64 = 23; 
-    // First, we construct an executor environment
-    //let env = ExecutorEnv::builder().build().unwrap();
-    let env = ExecutorEnv::builder()
-    // Send a & b to the guest
-    .add_input(&to_vec(&a).unwrap())
-    .add_input(&to_vec(&b).unwrap())
-    .build()
-    .unwrap();
+    // Pick two numbers
+    let (receipt, _) = multiply_factors(17, 23);
 
+    // Here is where one would send 'receipt' over the network...
 
-    // TODO: add guest input to the executor environment using
-    // ExecutorEnvBuilder::add_input().
-    // To access this method, you'll need to use the alternate construction
-    // ExecutorEnv::builder(), which creates an ExecutorEnvBuilder. When you're
-    // done adding input, call ExecutorEnvBuilder::build().
-
-    // For example:
-    // let env = ExecutorEnv::builder().add_input(&vec).build().unwrap();
-
-    // Next, we make an executor, loading the (renamed) ELF binary.
-    let mut exec = default_executor_from_elf(env, MULTIPLY_ELF).unwrap();
-
-    // Run the executor to produce a session.
-    let session = exec.run().unwrap();
-
-    // Prove the session to produce a receipt.
-    let receipt = session.prove().unwrap();
-
-    // TODO: Implement code for transmitting or serializing the receipt for
-    // other parties to verify here
-
-    // Optional: Verify receipt to confirm that recipients will also be able to
-    // verify your receipt
-    receipt.verify(MULTIPLY_ID).unwrap();
+    // Verify receipt, panic if it's wrong
+    receipt.verify(MULTIPLY_ID).expect(
+        "Code you have proven should successfully verify; did you specify the correct image ID?",
+    );
 }
